9df924308db155dd8338cba8475105948ec2eb97
Faran Javed <faran.code@gmail.com>
2026-06-05 00:50:12 +0500

    fix: tablelite + tableComp styles

--- client/packages/lowcoder/src/comps/comps/tableLiteComp/styles/CellStyles.ts
+++ client/packages/lowcoder/src/comps/comps/tableLiteComp/styles/CellStyles.ts
@@ -7,7 +7,7 @@ export const CellStyleProvider = styled.div<{
   .ant-table-tbody > tr > td {
     background: ${props => props.$rowStyle?.background || '#ffffff'};
     color: ${props => props.$rowStyle?.color || 'rgba(0, 0, 0, 0.85)'};
-    border-color: ${props => props.$rowStyle?.borderColor || '#f0f0f0'};
+    border-color: ${props => props.$rowStyle?.border || '#f0f0f0'};
     /* padding: ${props => props.$rowStyle?.padding || '12px 16px'}; */
     ${props => props.$rowStyle?.customCSS || ''}
   }
